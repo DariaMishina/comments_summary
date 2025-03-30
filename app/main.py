31b04aab14203c6f6a7d3912d95afc4116bdb98c
darya mishina <daria.mishina@bp.com>
2025-03-30 18:50:48 +0300

    add monitoring

--- app/main.py
+++ app/main.py
@@ -5,11 +5,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api import router as api_router
 from app.config import DEBUG
 
 app = FastAPI(debug=DEBUG)
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(api_router)
 
